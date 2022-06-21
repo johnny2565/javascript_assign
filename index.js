866ebcfaf67cfce9e13061ee45d22570dd23eefc
Johnny2565 <65639412+johnny2565@users.noreply.github.com>
2022-06-21 05:11:06 +0100

    added files

---
+++ index.js
@@ -0,0 +1,7 @@
+let name = document.querySelector("#name");
+let height = document.querySelector("#height");
+let country = document.querySelector("#country");
+
+name.innerHTML = "John Okpo";
+height.innerHTML = "1.7meters";
+country.innerHTML = "Nigeria";
